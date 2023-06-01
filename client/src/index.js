c20862bfa0ef0c1b5f03dd23cd12160003c4b3a5
Mustafa0503 <113068407+Mustafa0503@users.noreply.github.com>
2023-06-01 01:57:13 -0400

    Test set ups

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
-
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
