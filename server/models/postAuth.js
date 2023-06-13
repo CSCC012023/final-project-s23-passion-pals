384172862dee73d7214d819c8be0d54ea7d92a3c
Nishu <nishu.sheth@mail.utoronto.ca>
2023-06-13 19:26:59 -0400

    added basic backend for diplaying and editing profile information and eventcard(enrolling & unenrolling

--- server/models/postAuth.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-const newSchemaForLogin=new mongoose.Schema({
-    email: String,
-    password: String
-})
-
-const userAuthentication = mongoose.model("Users",newSchemaForLogin)
-export default userAuthentication;
