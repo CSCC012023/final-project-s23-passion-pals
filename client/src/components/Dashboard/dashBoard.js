50098c0a4925d56b48d4f61d8e52e7b0a1470457
Roger Zhao <78385317+zhaoroger@users.noreply.github.com>
2023-07-07 04:12:38 -0400

    [PP-13]-dashboard welcomes user and shows their currently enrolled events

--- client/src/components/Dashboard/dashBoard.js
+++ client/src/components/Dashboard/dashBoard.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
-
+import "./dashBoard.css";
 
 export default function Dashboard() {
   const [user, setUser] = useState(null);
   const userId = localStorage.getItem('userId');
+  const [eventIds, setEventIds] = useState([]);
+  const [events, setEvents] = useState([]);
 
   useEffect(() => {
     axios
@@ -16,15 +17,63 @@ export default function Dashboard() {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [userId]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/getUsers?userId=${userId}`)
+      .then(response => {
+        const user = response.data;
+        if (user) {
+          setEventIds(user.enrolledEvents);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [userId]); //is there a way to merge these useEffect
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/events')
+      .then(response => {
+        const filteredEvents = response.data.filter(event => eventIds.includes(event._id));
+        setEvents(filteredEvents);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [eventIds]);
+
   return (
+    <div className='dashboard'>
+      <div className='dash-head'>
+        <h1 className='welcome-text'>
+          Hello {user && user.fname}!
+        </h1>
+      </div>
+      <div className='dash-body'>
+        <h1>
+          You Have Signed Up For:
+        </h1>
 
-    <div>
-      <p>
-        Hi {user && user.fname}!
-      </p>
-
+        {events.length === 0 ? (
+          <p>No events found. Click Find Events to :&#41;</p>
+        ) : (
+          events.map(event => (
+            <div key={event._id} className="event-card">
+              <div className="event-image-container">
+                <img src={event.eventImage} alt="Event" className="event-image" />
+              </div>
+              <div className="event-content">
+                <p className="event-name">Name: {event.eventName}</p>
+                <p className="event-location">Location: {event.eventLocation}</p>
+                <p className="event-date">Date: {event.eventDate}</p>
+                <p className="event-price">Price: {event.eventPrice}</p>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
     </div>
-
   );
 }
