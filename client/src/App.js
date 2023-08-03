200a87c9d03194867421ed7279f8aaac333cc5ca
Roger Zhao <78385317+zhaoroger@users.noreply.github.com>
2023-08-02 22:33:42 -0400

    66-Change the navbar UI. Also fixed navbar not working on the friends page

--- client/src/App.js
+++ client/src/App.js
@@ -26,8 +26,7 @@ const App = () => {
     location.pathname === "/" ||
     location.pathname === "/select" ||
     location.pathname === "/selectEdit" ||
-    location.pathname === "/loading" ||
-    location.pathname === "/friend";
+    location.pathname === "/loading";
 
   const PrivateRoute = ({ element, path }) => {
     return isLoggedIn ? (
